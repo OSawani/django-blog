6af4b3dffca7d55bd83f88613224ca31b72113be
OSawani <DELETED-1699439824325>
2024-05-11 21:25:57 +0000

    env variables and config vars

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d-@0!u$@wgd!#4vlu0sw^8ibnsj8k+e@$0zpgimnz@q5hn&_y%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
