7f15801ce4841373ff258dd19f83c0a1eef76a2c
OSawani <DELETED-1699439824325>
2024-05-11 18:24:23 +0000

    set debug back to true

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d-@0!u$@wgd!#4vlu0sw^8ibnsj8k+e@$0zpgimnz@q5hn&_y%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-osawani-django-blog-ll55iu87eo.us1.codeanyapp.com', '.herokuapp.com']
 
