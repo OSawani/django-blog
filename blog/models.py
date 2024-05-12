e0a78bf399f8ec9600c451fcb250d167b27d9f5e
OSawani <DELETED-1699439824325>
2024-05-12 20:02:40 +0000

    added the comment model and registered it to admin

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,12 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+
+class Comment(models.Model):
+    APPROVAL = ((0, "Pending Approval"), (1, "Approved"))
+    author = models.ForeignKey(User, on_delete=models.CASADE, related_name="user_comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(choices=APPROVAL, default=0)
+    created_on = models.DateTimeField(auto_add_on=True)
