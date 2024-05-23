32b30e7c0283a23682b1f2dd7a3c0bf7180c04c0
OSawani <DELETED-1699439824325>
2024-05-23 13:14:33 +0000

    wiring the POST request handler to the view

--- about/admin.py
+++ about/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 
 admin.site.register(About)
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
