46c9fff816b14f2f46f32bd0ca3bdddf56916925
OSawani <DELETED-1699439824325>
2024-05-23 14:10:25 +0000

    added comment CRUD

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-osawani-django-blog-yrss3wv8bv.us1.codeanyapp.com', '.herokuapp.com']
 
